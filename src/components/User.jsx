35264d28110d53f7199a0fb78cb6f2ff181758b5
Michel Kabwe <mikael.tallbo@webexpress.se>
2023-01-07 20:50:46 +0100

    delete request + id working

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,25 +1,47 @@
-import React, {useState} from 'react'
+import React, {useState , useEffect} from 'react'
 import axios from 'axios'
 import '../styles/User.css'
 
 
 const User = ({user}) => {
-    const [userDeleted, setUserDeleted]= useState(false)
 
-
-    const deleteUser = (id) => {
-        axios.delete(`https://63b4536e0f49ecf50887d537.mockapi.io/register/${id}`)
-        setUserDeleted(true);
-
+    //const [userDeleted, setUserDeleted]= useState(false)
+    const [deleteUserId, setdeleteUserId] = useState(false);
+  
+    const deleteUser = async (id) => {
+        //axios.delete(`http://localhost:4000/log/userlist/${id}`);
+    
+        axios.delete('http://localhost:4000/log/userlist/'+id,{
+            params: {id:id}
+        })
+        .then((res) => { 
+            console.log(res)
+            setdeleteUserId(true)
+        })
+  
     }
+        
+        //setdeleteUserId(
+            //deleteUserId.filter((user) => {
+                //return user.id !== id;
+            //})
+        //)
+
+        
+       
+        //setUserDeleted(true);
+    
+
+
     return (
-        userDeleted ? null : (  
+       deleteUserId ? null : (  
         <div className="user-info" key={user.id}>
             <p className="user-id">User Id: {user.id}</p>
             <p className="user-email">Email: {user.email}</p>
             <button onClick={()=>deleteUser(user.id)} className="delete-btn">Delete</button>  
         </div>
-    ))
+    )
+    )
 }
 
 export default User
